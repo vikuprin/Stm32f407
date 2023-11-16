9efc983d6e286a27804375a4055ccf60b39054ae
vikuprin <106210586+vikuprin@users.noreply.github.com>
2023-11-16 12:17:44 +0700

    new dns

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -83,22 +83,15 @@ void dns_callback(const char *name, const ip_addr_t *ipaddr, void *callback_arg)
     {
     	DEBUG_MAIN("name = %s\n", name);
     	DEBUG_MAIN("ip = %s\n", ipaddr_ntoa(ipaddr));
-    	sprintf(mdns_name, "cityair350%d.local", mdns_id);
+    	sprintf(mdns_name, "cityair350%d", mdns_id);
     	mdns_id++;
     	mdns_search = false;
     }
     else
     {
     	DEBUG_MAIN("Not found dns ip %s\n", mdns_name);
-    	char *step = mdns_name;
-    	while (*step != '.')
-    	{
-    		step++;
-    	}
-		*step = '\0';
 		mdns_resp_add_netif(&gnetif, mdns_name, 120);
-		*step = '.';
-		mdns_resp_add_service(&gnetif, "lwip.local", "_http", DNSSD_PROTO_TCP, 80, 3600, srv_txt, NULL); //mdns_name
+		mdns_resp_add_service(&gnetif, mdns_name, "_http", DNSSD_PROTO_TCP, 80, 3600, srv_txt, NULL); //mdns_name
 		mdns_set = true;
 		mdns_search = false;
     }
@@ -218,7 +211,7 @@ void StartDefaultTask(void const * argument)
   }
   #if DNS == 1
   mdns_name = malloc(80);
-  sprintf(mdns_name, "cityair350.local");
+  sprintf(mdns_name, "cityair350");
   ip_addr_t addr;
   while (mdns_set == false)
   {
